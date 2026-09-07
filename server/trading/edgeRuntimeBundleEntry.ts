70595344cc3913e4b460a2f3091210a429615c1f
Hanseo Kim <122995222+hanul442@users.noreply.github.com>
2026-09-07 18:22:02 +0900

    sprint 7: add edge runtime bundle entry

---
+++ server/trading/edgeRuntimeBundleEntry.ts
@@ -0,0 +1,5 @@
+export { paperLoopController } from './paperLoop';
+export { paperTradingSession } from './paperSession';
+export { tradingEvidenceStore } from './evidenceStore';
+export { tradeCaseStore } from './tradeCaseStore';
+export { buildExternalTradingEvidence } from '../../src/trading/evidenceIngestion';
