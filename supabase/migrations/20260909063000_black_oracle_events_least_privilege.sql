cb6e1fab21475792dffb37ffaea300931544c737
Hanseo Kim <122995222+hanul442@users.noreply.github.com>
2026-09-09 15:41:48 +0900

    chore: remove superseded event ledger grant migration filename

--- supabase/migrations/20260909063000_black_oracle_events_least_privilege.sql
+++
@@ -1,9 +0,0 @@
-begin;
-
-revoke update, delete, truncate on table public.black_oracle_events from service_role;
-revoke update, delete, truncate on table public.black_oracle_events from authenticated;
-revoke update, delete, truncate on table public.black_oracle_events from anon;
-
-grant select, insert on table public.black_oracle_events to service_role;
-
-commit;
